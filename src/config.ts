153526aefbed89d56228274bca914bc3788df717
asashiki <asashiki.qianyishi@gmail.com>
2025-01-02 21:25:46 +0800

    改上传测试

--- src/config.ts
+++ src/config.ts
@@ -7,21 +7,21 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  title: 'Asashiki',
+  subtitle: '浅仪式的个人博客',
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 150,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/green-miku.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      enable: true,         // Display the credit text of the banner image
+      text: 'ガード下のメニューー歩く人',              // Credit text to be displayed
+      url: 'https://www.pixiv.net/artworks/121764304'                // (Optional) URL link to the original artwork or artist's page
     }
   },
   toc: {
@@ -29,11 +29,16 @@ export const siteConfig: SiteConfig = {
     depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+     {
+       src: '\favicon\圆角-莉赛特.jpg',    // Path of the favicon, relative to the /public directory
+       theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+       sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+     },
+     {
+      src: '\favicon\\圆角-莉赛特01.jpg',    // Path of the favicon, relative to the /public directory
+      theme: 'dark',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
@@ -43,34 +48,34 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      name: '时间胶囊',
+      url: 'https://timeline.asashiki.dynv6.net/',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/莉赛特01.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: '浅仪式',
+  bio: 'あ、海はどこまでも続いてね。           ——恋狱月狂病',
   links: [
     {
       name: 'Twitter',
       icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://twitter.com/asashiki_',
     },
     {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      name: 'Telegram',
+      icon: 'fa6-brands:telegram',
+      url: 'https://t.me/asashiki_timeline',
     },
     {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      name: 'RSS',
+      icon: 'mingcute:rss-2-fill',
+      url: 'https://timeline.asashiki.dynv6.net/rss.xml',
     },
   ],
 }
---
+++ src/content/posts/my-first-article.md
@@ -0,0 +1,77 @@
+---
+title: 测试标题测试
+published: 2025-01-02
+description: '测试网站发布'
+image: '/src/assets/images/demo-avatar.png'
+tags: [tag测试,Test]
+category: '测试'
+draft: false 
+---
+# 这是一个一级级标题
+
+测试一下markdown语法,感觉都忘完了。
+
+~~划线~~  _斜体_ **粗体**  `高亮`
+- 点
+- 二
+- 第二级测试
+    - 测试
+    - 测试
+
+> 块引用
+>这样写
+
+## h2头
+
+这是一个列表:
+
+1. 第一列
+2. 第二列
+3. 第三列
+    1. 二级第一列
+    2. 二级第二列
+
+
+```
+define foobar() {
+    print "Welcome to flavor country!";
+}
+```
+```python
+import time
+# Quick, count to ten!
+for i in range(10):
+    # (but not *too* quick)
+    time.sleep(0.5)
+    print i
+```
+
+### h3
+
+普通复制表:
+
+    这样子做
+    感觉还不错哦
+
+
+做一个嵌套表格:
+
+1. 首先准备:
+    - 🥕
+    - 🍎
+    - 🍌
+2. 煮一些水
+3. 遵循以下做法:
+
+        这是一个块儿
+        是连着的
+        就像这样
+    
+空一行是这样的
+
+这是一个链接[哔哩哔哩](www.bilibili.com),更改为一个本地存档[Markdown](local-doc.html),转换成[当前文章的标题](#这是一个一级级标题). 这是一个注脚[^1].
+
+[^1]: 注脚内容在这里
+
+---
+
